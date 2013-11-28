b838a7b69bc3bed4920cce444aaca2a1a277db22
Krzysztof Skoracki <k.skoracki@gmail.com>
2013-11-28 01:06:57 +0100

    Initial import.

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.contrib.project import rsync_project
+from fabric.api import run
+
+
+def sync_files():
+	rsync_project(remote_dir='/var/www/vhosts/esenti.net/ldpoznan',
+		local_dir='ldpoznan/',
+		exclude=['local_settings.py', '*.pyc', '*.wsgi', '*/static/*'],
+		delete=True,
+		extra_opts='')
+
+def reload_wsgi():
+	run('touch /var/www/vhosts/esenti.net/ldpoznan/ldpoznan.wsgi')
+
+def deploy():
+	sync_files()
+	reload_wsgi()
---
+++ ldpoznan/core/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from django.conf import settings
+
+import views
+
+urlpatterns = patterns('',
+	url(r'^$', views.index, name='index'),
+)
