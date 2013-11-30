eb5d7c62eb6f49682a94d5de6769ca872df05ab9
Krzysztof Skoracki <k.skoracki@gmail.com>
2013-11-30 02:04:12 +0100

    Added registration form.

--- ldpoznan/core/views.py
+++ ldpoznan/core/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render, redirect
+from django import forms
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+
+class RegistrationForm(forms.Form):
+	name = forms.CharField(label=_('Name'))
+	mail = forms.EmailField(label=_('Mail'), required=False)
+
 
 def index(request):
-	return render(request, 'core/index.html')
+
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(form.cleaned_data['name'], form.cleaned_data['mail'], 'pass')
+			return redirect('index')
+	else:
+		form = RegistrationForm()
+	return render(request, 'core/index.html', {'form': form})
